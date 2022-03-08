06fd73eb193b2041fb65db9d99b7a8261207291c
Joaquin Grez <joaquingrez@gmail.com>
2022-03-08 12:45:45 -0500

    Setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('readme.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cb-wrapper-dynamodb',
+    install_requires=[
+        'boto3',
+    ],
+    version='0.0.1',
+    author='Joaquin Grez',
+    author_email='joaquingrez@consiliumbots.com',
+    description=(
+        "Thin wrapper for DynamoDB's API"),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ConsiliumBots/cb-wrapper-dynamodb',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.9',
+)
