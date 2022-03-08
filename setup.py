380b9f8c0ebd0e3f5e7eba624b6016f0406a97db
Joaquin Grez <joaquingrez@gmail.com>
2022-03-08 13:25:28 -0500

    Renamed for imports

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("readme.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cb-wrapper-dynamodb",
+    name="dynamodb",
     install_requires=[
         "boto3",
     ],
